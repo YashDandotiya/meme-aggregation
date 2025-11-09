f39af436816190d82477ec78cdfb07c33a1e1021
YashDandotiya <138976774+YashDandotiya@users.noreply.github.com>
2025-11-09 19:53:43 +0530

    added package version for this task with given tech stack

---
+++ jest.config.js
@@ -0,0 +1,20 @@
+module.exports = {
+  preset: 'ts-jest',
+  testEnvironment: 'node',
+  roots: ['<rootDir>/tests'],
+  testMatch: ['**/__tests__/**/*.ts', '**/?(*.)+(spec|test).ts'],
+  collectCoverageFrom: [
+    'src/**/*.ts',
+    '!src/**/*.d.ts',
+    '!src/server.ts'
+  ],
+  coverageThreshold: {
+    global: {
+      branches: 30,
+      functions: 50,
+      lines: 60,
+      statements: 60
+    }
+  },
+  testTimeout: 30000
+};
